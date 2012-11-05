2bf899265e3416864828ec0f13c43de910e8b1b4
David Cai <w.david.cai@gmail.com>
2012-11-05 14:27:19 -0800

    ChartsPage iteration 1.

--- Baby/src/baby/pages/scrapbook/ChartsPage.java
+++ Baby/src/baby/pages/scrapbook/ChartsPage.java
@@ -1,20 +1,77 @@
 package baby.pages.scrapbook;
 
+import java.util.List;
+import java.util.UUID;
+
+import samoyan.controls.TwoColFormControl;
+import baby.database.Measure;
+import baby.database.MeasureStore;
+import baby.database.Mother;
+import baby.database.MotherStore;
+import baby.database.Stage;
 import baby.pages.BabyPage;
 
 public class ChartsPage extends BabyPage
 {
 	public final static String COMMAND = BabyPage.COMMAND_SCRAPBOOK + "/charts";
 	
-	@Override
-	public String getTitle() throws Exception
-	{
-		return getString("scrapbook:Charts.Title");
-	}
+	public final static String PARAM_VALUE_PREFIX = "value_";
 	
 	@Override
 	public void renderHTML() throws Exception
 	{
-		write("This is a test"); // !$!
+		write("<h2>");
+		write(getString("scrapbook:Charts.RecordMeasures"));
+		write("</h2>");
+		
+		Mother mom = MotherStore.getInstance().loadByUserID(getContext().getUserID());
+//		List<UUID> recIDs = MeasureRecordStore.getInstance().getByUserID(getContext().getUserID());
+		
+		List<UUID> measureIDs = MeasureStore.getInstance().getAll(true);
+		measureIDs.addAll(MeasureStore.getInstance().getAll(false));
+		
+		writeMeasures(measureIDs);
+	}
+	
+	private void writeMeasures(List<UUID> measureIDs) throws Exception
+	{
+		Mother mom = MotherStore.getInstance().loadByUserID(getContext().getUserID());
+		Stage stage = mom.getPregnancyStage();
+		
+		TwoColFormControl twoCol = new TwoColFormControl(this);
+		
+		int i = 0;
+		for (UUID id : measureIDs)
+		{
+			Measure measure = MeasureStore.getInstance().load(id);
+			
+			if ((measure.isForPreconception() && stage.isPreconception()) || 
+				(measure.isForPregnancy() && stage.isPregnancy()) || 
+				(measure.isForInfancy() && stage.isInfancy())) 
+			{
+				writeMeasure(measure, twoCol, mom.isMetric(), i);
+				i++;
+			}
+		}
+		
+		twoCol.render();
+	}
+	
+	private void writeMeasure(Measure measure, TwoColFormControl twoCol, boolean metric, int index) throws Exception
+	{
+		twoCol.writeRow(measure.getLabel());
+
+		Integer min = metric ? measure.getMetricMin() : measure.toImperial(measure.getMetricMin());
+		Integer max = metric ? measure.getMetricMax() : measure.toImperial(measure.getMetricMax());
+		
+		twoCol.writeNumberInput(PARAM_VALUE_PREFIX + index, min, 16, min, max);
+		twoCol.write("&nbsp;");
+		twoCol.writeEncode(metric ? measure.getMetricUnit() : measure.getImperialUnit());
+	}
+	
+	@Override
+	public String getTitle() throws Exception
+	{
+		return getString("scrapbook:Charts.Title");
 	}
 }
